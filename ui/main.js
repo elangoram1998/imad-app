83fb32491152b4a97fb3057d506aab91707d637b
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2017-08-25 19:12:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main text');
 element.innerHTML='new value';
+var img=doument.getElementById('madi');
+img. onclick=function()
+{
+    img.stylee.marginleft='100px';
+};
